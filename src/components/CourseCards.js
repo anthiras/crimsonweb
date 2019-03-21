f80871f1bcdcb3fc8bc1a1c043edf92907b36ef7
Anders Thirsgaard Rasmussen <atr@atr.dk>
2019-03-21 20:26:59 +0100

    Added week numbers to course list

--- src/components/CourseCards.js
+++ src/components/CourseCards.js
@@ -2,18 +2,43 @@ import React from 'react';
 import CourseListItem from '../containers/CourseListItem'
 import { withTranslation } from 'react-i18next';
 import { Pagination } from './Utilities';
+import moment from 'moment';
+
+const groupCoursesByWeek = courses => {
+	let coursesByWeek = [];
+	let currentKey = null;
+	for (let i = 0; i < courses.length; i++) {
+		let course = courses[i];
+		let startsAt = moment(new Date(course.startsAt));
+		let week = startsAt.week();
+		let key = startsAt.year() + '-' + startsAt.week();
+		if (key !== currentKey) {
+			coursesByWeek.push({ key: key, week: week, courses: []});
+			currentKey = key;
+		}
+		coursesByWeek[coursesByWeek.length-1].courses.push(course);
+	}
+	return coursesByWeek;
+}
 
 const CourseCards = ({ t, courses, page, lastPage, list }) => {
 	if (courses.length === 0)
 		return (<p className="lead text-center text-muted my-5">{t('courses:noCoursesFound')}</p>);
 
+	const coursesByWeek = groupCoursesByWeek(courses);
+
 	return (
 		<React.Fragment>
-		    <div className="card-columns">
-		        {courses.map(course =>
-		            <CourseListItem course={course} key={course.id} />
-		        )}
-		    </div>
+			{coursesByWeek.map(courseWeek => 
+				<React.Fragment key={courseWeek.key}>
+					<h4>{t('common:week')} {courseWeek.week}</h4>
+					    <div className="card-columns">
+					        {courseWeek.courses.map(course =>
+					            <CourseListItem course={course} key={course.id} />
+					        )}
+					    </div>
+		    	</React.Fragment>
+		    )}
 		    <Pagination page={page} lastPage={lastPage} urlForPage={(page) => '/courses/'+list+'/'+page} />
 	    </React.Fragment>
 	);
